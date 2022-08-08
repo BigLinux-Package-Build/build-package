3386cdb37f3701b9c4308dbac19a071b18001ccc
Barnabe <barnabedikartola@gmail.com>
2022-08-08 03:15:29 -0300

    remove md5.sh

--- md5.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gitfolder=$(find -type f -name PKGBUILD | sed 's/PKGBUILD//')
-cd $gitfolder
-file=$(find -type f -iname "*.zst" | cut -d "/" -f2)
-for i in *.zst *.sig; do test -e $i && md5sum $i >> "$file"".md5"; done
-exit 0
